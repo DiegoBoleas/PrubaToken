094561825c031236c2e37d75b9f540f98a5d1bc7
diegoBoleas <diego.boldel@1DAW-10.iesluisbraille.es>
2025-02-12 10:26:37 +0100

    Prueba 2

--- src/token/Token.java
+++ src/token/Token.java
@@ -7,7 +7,8 @@ public class Token {
     public static void main(String[] args) {
         
         System.out.println("Prueba git hub");
-              
+        
+        System.out.println("Prueba 2");  
     }
     
 }
